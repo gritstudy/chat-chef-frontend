cc17ccc29ade6e67ec9c6a88f622d075e77458d8
gritstudy <hrlee716@gmail.com>
2025-07-11 15:56:29 +0900

    title updated

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const title = ({ mainTitle, subTitle }) => {
   return (
     <div className="px-2 pt-6">
-      <h1 className="text-4.5xl font-black text-white">{mainTitle}</h1>
+      <h1 className="text-3.5xl font-black text-white">{mainTitle}</h1>
       {subTitle && (
-        <span className="block text-sm mt-3 text-white break-keep pr-9">
+        <span className="block text-sm mt-3 text-black break-keep pr-9">
           {subTitle}
         </span>
       )}
